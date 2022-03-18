0e904cbe05ac97a98947fe02b80b4781addbce13
unknown <jaseelmanamulli@gmail.com>
2022-03-18 11:35:11 +0530

    seconds Commit

--- src/sub_screen/a_bookings.js
+++ src/sub_screen/a_bookings.js
@@ -4,44 +4,26 @@ import BodyTopbar from "../widget/body_topbar";
 import MyTable from "../widget/table";
 
 export default function ABooking({ props }) {
-  const { sbookingsT, tbookingsT, booking, spage } = props.state;
-  const { sbookings, tbookings } = props.state;
+  const { bookings, bookingsT, booking } = props.state;
   const setState = (v) => props.setState(v);
 
-  const titles = [
-    { title: "Setes Bookings", count: "" },
-    { title: "Team Bookings", count: "" },
-  ];
+  const titles = [{ title: "Setes Bookings", count: "" }];
 
   return (
     <React.StrictMode>
       <div className="cm1_page_title">Trufs</div>
       <BodyTopbar titles={titles} onclick={(v) => setState({ spage: v })} />
-      {spage === 0 ? (
-        <MyTable
-          nosearch
-          nofilter
-          title="Setes Bookings"
-          titles={[
-            { name: "Title", cl: "tr5" },
-            { name: "Date", cl: "tr4" },
-          ]}
-          onclick={(v) => setState({ booking: sbookings[v] })}
-          items={sbookingsT}
-        />
-      ) : (
-        <MyTable
-          nosearch
-          nofilter
-          title="Team Bookings"
-          titles={[
-            { name: "Title", cl: "tr5" },
-            { name: "Date", cl: "tr4" },
-          ]}
-          onclick={(v) => setState({ booking: tbookings[v] })}
-          items={tbookingsT}
-        />
-      )}
+      <MyTable
+        nosearch
+        nofilter
+        title="Setes Bookings"
+        titles={[
+          { name: "Title", cl: "tr5" },
+          { name: "Date", cl: "tr4" },
+        ]}
+        onclick={(v) => setState({ booking: bookings[v] })}
+        items={bookingsT}
+      />
       {booking !== null ? <BookingPopup props={props} /> : null}
     </React.StrictMode>
   );
